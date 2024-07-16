6d5d570012ffc1c134a72da02d88907b70ee6475
Marvin antonio Barrera Trigueros <marvin022021@outlook.es>
2024-07-15 22:04:54 -0600

    Agregar archivos de proyecto.

---
+++ AppBlazorCRUD/Data/CRUDblazordbContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace AppBlazorCRUD.Data
+{
+    public partial class CRUDblazordbContext : DbContext
+    {
+        public CRUDblazordbContext()
+        {
+        }
+
+        public CRUDblazordbContext(DbContextOptions<CRUDblazordbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Clientes { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.ToTable("Cliente");
+
+                entity.Property(e => e.Apellido)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Nombre)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ AppBlazorCRUD/Data/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppBlazorCRUD.Data
+{
+    public partial class Cliente
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre no puede tener más de 50 caracteres")]
+        public string Nombre { get; set; } = null!;
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(50, ErrorMessage = "El apellido no puede tener más de 50 caracteres")]
+        public string Apellido { get; set; } = null!;
+        [EmailAddress(ErrorMessage = "El formato del email no es válido")]
+        public string? Email { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? FechaNacimiento { get; set; }
+    }
+}
